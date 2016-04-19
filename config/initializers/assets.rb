9edfe92121fcf97ec81cec32780a0ef69dd66659
Ben A. Morgan <ben@benmorgan.io>
2016-04-18 17:49:02 -0700

    update asset locations

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,7 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts', 'nxtleveldocs')
-Rails.application.config.assets.precompile += %w( screen.css print.css )
+Rails.application.config.assets.precompile += %w(
+  nxtleveldocs/navbar.png
+  nxtleveldocs/screen.css
+  nxtleveldocs/print.css
+)
